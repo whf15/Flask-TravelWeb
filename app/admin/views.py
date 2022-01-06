6c8f698fc6a840dd0e9b3c5593f79224f437390e
whf <1211368233@qq.com>
2022-01-06 22:58:37 +0800

    login

--- app/admin/views.py
+++ app/admin/views.py
@@ -5,11 +5,51 @@ from datetime import datetime
 from app import db
 from . import admin
 from flask import render_template, redirect, url_for, flash, session, request, g, abort,make_response,current_app
-from app.admin.forms import LoginForm
+from app.admin.forms import LoginForm,PwdForm,AreaForm,ScenicForm,TravelsForm
 from app.models import Admin,Adminlog,Oplog,Userlog,Area,User,Suggestion,Scenic,Travels
 from werkzeug.utils import secure_filename
 from sqlalchemy import or_ , and_
 from functools import wraps
+
+
+def admin_login(f):
+    """
+    登录装饰器
+    """
+
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        if "admin" not in session:
+            return redirect(url_for("admin.login", next=request.url))
+        return f(*args, **kwargs)
+
+    return decorated_function
+
+def addOplog(reason):
+    oplog = Oplog(
+        admin_id=session["admin_id"],
+        ip=request.remote_addr,
+        reason=reason
+    )
+    db.session.add(oplog)
+    db.session.commit()
+
+def gen_rnd_filename():
+    return datetime.now().strftime("%Y%m%d%H%M%S") + str(uuid.uuid4().hex)
+
+def change_filename(filename):
+    """
+    修改文件名称
+    """
+    fileinfo = os.path.splitext(filename)
+    filename =  gen_rnd_filename() + fileinfo[-1]
+    return filename
+
+
+@admin.route("/")
+@admin_login
+def index():
+    return render_template("admin/index.html")
 
 @admin.route("/login/", methods=["GET", "POST"])
 def login():
@@ -36,4 +76,34 @@ def login():
         db.session.commit() # 提交数据
         return redirect(url_for("admin.index")) # 返回后台主页
 
-    return render_template("admin/login.html",form=form)   
+    return render_template("admin/login.html",form=form)    
+
+@admin.route('/logout/')
+@admin_login
+def logout():
+    """
+    后台注销登录
+    """
+    session.pop("admin", None)
+    session.pop("admin_id", None)
+    return redirect(url_for("admin.login"))
+
+@admin.route('/pwd/',methods=["GET","POST"])
+@admin_login
+def pwd():
+    """
+    后台密码修改
+    """
+    form = PwdForm()
+    if form.validate_on_submit():
+        data = form.data
+        admin = Admin.query.filter_by(name=session["admin"]).first()
+        from werkzeug.security import generate_password_hash
+        admin.pwd = generate_password_hash(data["new_pwd"])
+        db.session.add(admin)
+        db.session.commit()
+        flash("修改密码成功，请重新登录！", "ok")
+        return redirect(url_for('admin.logout'))
+    return render_template("admin/pwd.html", form=form)
+
+
